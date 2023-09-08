2f033cb1491fab0a5835540c7c05393f9818fb2c
mikejbp <migueljosebp@gmail.com>
2023-09-08 19:29:53 -0400

    fix: Fix EmotionCache provider

--- src/theme/ThemeRegistry.tsx
+++ src/theme/ThemeRegistry.tsx
@@ -1,6 +1,6 @@
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
-import { NextAppDirEmotionCacheProvider, responsiveTheme } from '.';
+import { NextAppDirEmotionCacheProvider } from '@/app/EmotionCache';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { responsiveTheme } from '.';
 
 export function ThemeRegistry({ children }: { children: React.ReactNode }) {
   return (
